3920741c14653b318208dac52e775e69adcfc585
Shriyam Shrivastava <shrivastavashriyam2@gmail.com>
2026-07-22 11:27:02 +0530

    Dockerfile — containerise FastAPI app

--- practise.py
+++ practise.py
@@ -1,25 +1,7 @@
-from email import message
-from typing import TypedDict, Annotated
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
+from fastapi import FastAPI
 
+app = FastAPI()
 
-class AgentState(TypedDict):
-    messages: Annotated[list, add_messages]
-    next: str
-
-def router(state: AgentState):
-    if(state['next'] == "researcher"):
-        return "researcher" ##this just for example we are not wrtiing niode here 
-    else:
-        return END
-
-graph = StateGraph(AgentState)
-
-graph.add_conditional_edges('router', router)
-
-new_graph = graph.compile()
-
-new_graph.invoke({
-    "messages":[{"role":"user","content":"What is the battery capacity of the Z Fold7?"}]
-})
+@app.get('/')
+def say_hello():
+    return {"message":"Hello"}
